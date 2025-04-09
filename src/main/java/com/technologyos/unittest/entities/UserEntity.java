3653ef735b024939545ce14680c42a2a2f4f33da
ArmandoSJ <just-jauregui@hotmail.com>
2025-04-08 21:06:34 -0600

    Added user entity

--- src/main/java/com/technologyos/unittest/entities/UserEntity.java
+++ src/main/java/com/technologyos/unittest/entities/UserEntity.java
@@ -10,26 +10,26 @@ import java.io.Serializable;
 @Data
 @Table(name="users")
 public class UserEntity implements Serializable {
-    @Serial
-    private static final long serialVersionUID = 5313493413859894403L;
+   @Serial
+   private static final long serialVersionUID = 5313493413859894403L;
 
-    @Id
-    @GeneratedValue
-    private long id;
+   @Id
+   @GeneratedValue
+   private long id;
 
-    @Column(nullable=false, unique=true)
-    private String userId;
+   @Column(nullable = false, unique = true)
+   private String userId;
 
-    @Column(nullable=false, length=50)
-    private String firstName;
+   @Column(nullable = false, length = 50)
+   private String firstName;
 
-    @Column(nullable=false, length=50)
-    private String lastName;
+   @Column(nullable = false, length = 50)
+   private String lastName;
 
-    @Column(nullable=false, length=120, unique = true)
-    private String email;
+   @Column(nullable = false, length = 120, unique = true)
+   private String email;
 
-    @Column(nullable=false)
-    private String encryptedPassword;
+   @Column(nullable = false)
+   private String encryptedPassword;
 
 }
