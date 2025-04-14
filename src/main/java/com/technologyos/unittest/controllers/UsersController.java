d458b3f7bdcc47dd746d71d5b73ac12a60a66070
ArmandoSJ <just-jauregui@hotmail.com>
2025-04-13 21:09:15 -0600

    Added user controller

--- src/main/java/com/technologyos/unittest/controllers/UsersController.java
+++ src/main/java/com/technologyos/unittest/controllers/UsersController.java
@@ -1,8 +1,17 @@
 package com.technologyos.unittest.controllers;
 
+import com.technologyos.unittest.dtos.UserDto;
+import com.technologyos.unittest.dtos.request.UserDetailRequest;
+import com.technologyos.unittest.dtos.response.UserResponse;
 import com.technologyos.unittest.services.UsersService;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -10,4 +19,23 @@ import org.springframework.web.bind.annotation.*;
 public class UsersController {
    private final UsersService usersService;
 
+   @PostMapping
+   public UserResponse createUser(@RequestBody @Valid UserDetailRequest userDetails) throws Exception {
+      ModelMapper modelMapper = new ModelMapper();
+      UserDto userDto = new ModelMapper().map(userDetails, UserDto.class);
+
+      UserDto createdUser = usersService.createUser(userDto);
+
+      return modelMapper.map(createdUser, UserResponse.class);
+   }
+
+   @GetMapping
+   public List<UserResponse> getUsers(@RequestParam(value = "page", defaultValue = "0") int page,
+                                  @RequestParam(value = "limit", defaultValue = "2") int limit) {
+      List<UserDto> users = usersService.getUsers(page, limit);
+
+      Type listType = new TypeToken<List<UserResponse>>() {}.getType();
+
+      return new ModelMapper().map(users, listType);
+   }
 }
