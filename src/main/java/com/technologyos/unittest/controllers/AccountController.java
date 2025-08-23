3ff16baae1cd55e53992889b1f4f0d7f9f0ad95c
ArmandoSJ <just-jauregui@hotmail.com>
2025-08-22 21:24:14 -0600

    add initial user controller

---
+++ src/main/java/com/technologyos/unittest/controllers/AccountController.java
@@ -0,0 +1,17 @@
+package com.technologyos.unittest.controllers;
+
+import com.technologyos.unittest.models.Account;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/account")
+public class AccountController {
+
+   @GetMapping("/{id}")
+   public Account detail(@PathVariable(name = "id") long id){
+      return null;
+   }
+}
