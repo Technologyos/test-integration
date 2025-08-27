02ed516aa7ede7f869377fa1529d912d8e9f2d44
ArmandoSJ <just-jauregui@hotmail.com>
2025-08-26 21:59:23 -0600

    chore: add all args contructor at the user controller

--- src/main/java/com/technologyos/unittest/controllers/AccountController.java
+++ src/main/java/com/technologyos/unittest/controllers/AccountController.java
@@ -1,6 +1,7 @@
 package com.technologyos.unittest.controllers;
 
 import com.technologyos.unittest.models.Account;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/account")
+@AllArgsConstructor
 public class AccountController {
 
    @GetMapping("/{id}")
