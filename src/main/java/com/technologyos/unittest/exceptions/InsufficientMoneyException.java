36c0d2e1388554d2e3d35bc1c88c316e58e7dfc0
ArmandoSJ <just-jauregui@hotmail.com>
2025-09-08 21:56:35 -0600

    chore: add empty contructor in insuffcienMoneyException

--- src/main/java/com/technologyos/unittest/exceptions/InsufficientMoneyException.java
+++ src/main/java/com/technologyos/unittest/exceptions/InsufficientMoneyException.java
@@ -1,6 +1,8 @@
 package com.technologyos.unittest.exceptions;
 
 public class InsufficientMoneyException extends RuntimeException {
+   public InsufficientMoneyException(){}
+
    public InsufficientMoneyException(String message){
       super(message);
    }
