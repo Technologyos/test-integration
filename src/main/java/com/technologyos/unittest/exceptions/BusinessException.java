b64a065fbaa3e65c1bab7d7e824dd46d2fa74cca
ArmandoSJ <just-jauregui@hotmail.com>
2025-09-30 21:37:34 -0600

    feat: se agrega business exception

---
+++ src/main/java/com/technologyos/unittest/exceptions/BusinessException.java
@@ -0,0 +1,29 @@
+package com.technologyos.unittest.exceptions;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class BusinessException extends RuntimeException {
+   private Long id;
+   private String code;
+   private String detail;
+   private HttpStatus httpStatus;
+
+   public BusinessException(String code, String message, String detail, HttpStatus httpStatus) {
+      super(message);
+      this.code = code;
+      this.detail = detail;
+      this.httpStatus = httpStatus;
+   }
+
+   public BusinessException(String message, Throwable cause) {
+      super(message, cause);
+   }
+}
