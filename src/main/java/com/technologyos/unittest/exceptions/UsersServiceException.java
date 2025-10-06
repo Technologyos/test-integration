065ac496941b5d855bbc9dd818cda47bfefd6f52
ArmandoSJ <just-jauregui@hotmail.com>
2025-10-05 20:16:41 -0600

    chore: se actualiza user service exception

--- src/main/java/com/technologyos/unittest/exceptions/UsersServiceException.java
+++ src/main/java/com/technologyos/unittest/exceptions/UsersServiceException.java
@@ -1,8 +1,15 @@
 package com.technologyos.unittest.exceptions;
 
-public class UsersServiceException extends RuntimeException {
-   public UsersServiceException(){}
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class UsersServiceException extends RuntimeException {
    public UsersServiceException(String message) {
       super(message);
    }
