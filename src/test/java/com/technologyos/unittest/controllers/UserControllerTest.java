e284c0af7c781222fab65de70047692edb492643
ArmandoSJ <just-jauregui@hotmail.com>
2025-10-13 21:51:07 -0600

    chore: se actualiza el el nombre del user controler

--- src/test/java/com/technologyos/unittest/controllers/UserControllerTest.java
+++ src/test/java/com/technologyos/unittest/controllers/UserControllerTest.java
@@ -5,11 +5,9 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class UsersControllerTest {
+class UserControllerTest {
 
 }
