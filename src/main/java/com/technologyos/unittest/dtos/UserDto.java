46d1df7b7b6b8b67ac35eb3ce95acb90d32a4e87
ArmandoSJ <just-jauregui@hotmail.com>
2025-11-12 19:59:00 -0600

    chore: se actualiza el user dto

--- src/main/java/com/technologyos/unittest/dtos/UserDto.java
+++ src/main/java/com/technologyos/unittest/dtos/UserDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Data
@@ -12,6 +13,9 @@ import java.io.Serializable;
 @NoArgsConstructor
 @Builder
 public class UserDto implements Serializable {
+   @Serial
+   private static final long serialVersionUID = 7213423423859894403L;
+
     private String userId;
     private String firstName;
     private String lastName;
