b10bda4fcf321e3b0025016a818d78e9d250c0cd
ArmandoSJ <just-jauregui@hotmail.com>
2025-12-06 22:26:14 -0600

    chore: se agrega builder al objeto bank

--- src/main/java/com/technologyos/unittest/models/Bank.java
+++ src/main/java/com/technologyos/unittest/models/Bank.java
@@ -1,12 +1,18 @@
 package com.technologyos.unittest.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Bank {
    private List<Account> accounts = new ArrayList<>();
    private String name;
